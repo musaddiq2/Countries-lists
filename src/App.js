91cdae65d14ca9d2628050557afe9bb7c62d98b1
musaddiq2 <uzumakibaruto093@gmail.com>
2024-01-17 23:14:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useEffect, useState } from "react";
 function App() {
+  const [data, setData] = useState([]);
+  const getCountries = async () => {
+    try {
+      const res = await fetch("https://restcountries.com/v3.1/all");
+      const countries = await res.json();
+      setData(countries);
+    } catch (error) {
+      console.log("catch", error);
+    }
+  };
+  useEffect(() => {
+    getCountries();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="countryStyle">
+    
+      {data.map((item) => {
+        return (
+          <div className="country-card ">
+            <img src={item.flags.png} className="country" alt="country-flags" />
+            <h3>{item.continents}</h3>
+          </div>
+        );
+      })}
     </div>
   );
 }
---
+++ src/countriesList.js
@@ -0,0 +1,8 @@
+import React, { useEffect, useState } from "react";
+
+function countriesList() {
+ 
+  return <div>countriesList</div>;
+}
+
+export default countriesList;
